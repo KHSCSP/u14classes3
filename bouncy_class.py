0e115e63efbf00692a9066cdbe3d87505df69889
heyscottn <heyscottn@gmail.com>
2024-03-03 09:11:41 -0600

    mm

--- bouncy_class.py
+++ bouncy_class.py
@@ -3,23 +3,29 @@ import pygame, random, math
 class Bouncy:
     def __init__(self, x=100, y=100, size=10):
         pass
-    
+
+   
     def draw(self, screen):
         pass
     
 
     def move(self, screen):
         pass
-    
+
+
     def rand_move(self, screen):
         pass
+
 
     def tele_move(self, screen):
         pass
 
 
     # a helper function
-    # not a method because no instance will call this
+    # *not a method* because no instance will call this
+    # we will: Bouncy.rand_col()
+    # we will *not*: b.rand_col()
+    # it 'belongs' to the class, not an object 'itself'
     def rand_col():
         r = random.randint(0,255)
         g = random.randint(0,255)
